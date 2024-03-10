40228d8413a88c6bf3da61cdeeb6813beb24f83b
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2024-03-10 16:19:26 -0400

    create customer function

--- controllers/customerController.ts
+++ controllers/customerController.ts
@@ -13,10 +13,18 @@ const createCustomer = async (customerData: CustomerData) => {
   const city = formatString(customerData.city)
   const address = formatString(customerData.address)
   // check db for street address
-  const customerExists: boolean = (await Customer.exists({city: city, address: address})) === null;
+  const customerExists: boolean = (await Customer.exists({city: city, address: address})) !== null;
   // if address not in db - create new customer
-
-  // if address is in db - respond ('customer already exists');
+  try {
+    if (!customerExists){
+      console.log('customer does not exist');
+      console.log('save to database');
+    } else {
+      console.log('customer already exists');
+    }}
+    // if address is in db - respond ('customer already exists');
+    catch 
+      { throw new Error('error checking for duplicates in database') }
 }
 
 const fetchCustomer = async () => {}
