0dc8135c8ebcbafb5c7f35d2fe9ca4c594296a92
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2024-03-13 20:11:13 -0400

    update controller functions

--- controllers/customerController.ts
+++ controllers/customerController.ts
@@ -9,45 +9,75 @@ interface CustomerData {
   address: string,
 }
 
+
 const createCustomer =  async (req: Request, res: Response) => {
   try {
-  const {customerName, city, address}: CustomerData = req.body
-  const formattedCustomerName = formatString(customerName)
-  const formattedCity = formatString(city)
-  const formattedAddress = formatString(address)
-  // check db for street address
-  const customerExists: boolean = (await Customer.exists({city: city, address: address})) !== null;
-  // if address not in db - create new customer
-    if (!customerExists){
-      console.log('customer does not exist');
-      console.log('save to database');
-    } else {
-      console.log('customer already exists');
-    }}
-    // if address is in db - respond ('customer already exists');
-    catch 
-      { throw new Error('error checking for duplicates in database') }
+    const {customerName, city, address}: CustomerData = req.body
+    const formattedCustomerName = formatString(customerName)
+    const formattedCity = formatString(city)
+    const formattedAddress = formatString(address)
+    // check db for street address
+    const customerExists: boolean = (await Customer.exists({city: formattedCity, address: formattedAddress})) !== null;
+    // if address not in db - create new customer
+      if (!customerExists){
+        console.log('customer does not exist');
+        console.log('save to database');
+      } else {
+        console.log('customer already exists');
+      }}
+  catch 
+    { throw new Error('error checking for duplicates in database') }
 }
+
 
 const fetchCustomer = async (req: Request, res: Response) => {
   try {  
-    const address :string = req.body;
-    const data = await Customer.findOne({address: address});
+    const { address, city } = req.body;
+    const formattedAddress: string = formatString(address);
+    const formattedCity: string = formatString(city);
+    const data = await Customer.findOne({city: formattedCity, address: formattedAddress});
     return data;
-  } catch (error) {
+  } 
+  catch {
     res.status(500).send('Error fetching customer data');
   }
-
 }
 
-const updateCustomer = async (req: Request, res: Response) => {}
 
-const deleteCustomer = async (req: Request, res: Response) => {}
+const updateEquipment = async (req: Request, res: Response) => {
+  try {const { customerId, equipmentId } = req.params;
+    const equipmentUpdates: string = req.body;
+    const update = await Customer.findOneAndUpdate({_id: customerId, "equipment._id": equipmentId},
+      { $set: {"equipment.$": equipmentUpdates } }, { new: true }
+    );
+  } 
+  catch {
+      res.status(500).send('Error updating data');
+    }
+}
+
+
+const deleteCustomer = async (req: Request, res: Response) => {
+  const customerId = req.params; 
+  // check db for street address
+  const customerExists: boolean = (await Customer.exists({_id: customerId})) !== null;
+  if (!customerExists) {res.status(404).send('customer does not exist')}
+  else {
+    try{
+      await Customer.findByIdAndDelete({_id: customerId})
+      res.send('Successfully Deleted');
+    }
+    catch {
+      res.status(500).send('Delete attempt was unsuccessful - Server side error');
+    }
+  }
+}
+
 
 module.exports = {
   createCustomer,
   fetchCustomer,
-  updateCustomer,
+  updateEquipment,
   deleteCustomer
 }
 
--- models/customerModel.ts
+++ models/customerModel.ts
@@ -2,21 +2,23 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const customerSchema = new Schema ({
-  customerName: {type: String, index: true},
-  city: {type: String, index: true},
-  address: {type: String, index: true},
-  equipment: {
-    system: [{
-      name: String, 
+const EquipmentSchema = new Schema({
+  name: String, 
       type: String, 
       filters: {size: String, quantity: Number},
       belts: {size: String, quantity: Number},
       model: String,
       serial: String,
       installDate: Date,
-      equipmentNotes: [{note: String, date: Date}]}]
-  },
+      equipmentNotes: [{note: String, date: Date}]
+  }, {_id: true}
+);
+
+const customerSchema = new Schema ({
+  customerName: {type: String, index: true},
+  city: {type: String, index: true},
+  address: {type: String, index: true},
+  equipment: [EquipmentSchema], 
   locationNotes: [{note: String, date: Date}]
 })
 
