bebccd1fffec3da0693bf09494d4a27f1b4a0e71
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2024-03-15 09:44:01 -0400

    update schema details

--- models/customerModel.ts
+++ models/customerModel.ts
@@ -3,23 +3,23 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const EquipmentSchema = new Schema({
-  name: String, 
-      type: String, 
-      filters: {size: String, quantity: Number},
-      belts: {size: String, quantity: Number},
-      model: String,
-      serial: String,
-      installDate: Date,
-      equipmentNotes: [{note: String, date: Date}]
+  name: {type: String, required: true},
+  type: {type: String, required: true},
+  filters: {type: {size: String, quantity: Number}, required: false},
+  belts: {type: {size: String, quantity: Number}, required: false},
+  model: {type: String, required: false},
+  serial: {type: String, required: false},
+  installDate: {type: Date, required: false},
+  equipmentNotes: {type:[{note: String, date: Date}], required: false}
   }, {_id: true}
 );
 
 const CustomerSchema = new Schema ({
-  customerName: {type: String, index: true},
-  city: {type: String, index: true},
-  address: {type: String, index: true},
-  equipment: [EquipmentSchema], 
-  locationNotes: [{note: String, date: Date}]
+  customerName: {type: String, index: true, required: true},
+  city: {type: String, index: true, required: true},
+  address: {type: String, index: true, required: true},
+  equipment: {type:[EquipmentSchema], required: false}, 
+  locationNotes: {type:[{note: String, date: Date}], required: false}
 })
 
 const Customer = mongoose.model('Customer', CustomerSchema);
